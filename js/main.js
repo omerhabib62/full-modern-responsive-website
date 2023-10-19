3f5ba749d9db625f8e0ad105cf0bfa8d5ed4416c
Omer Habib <omer.habib62@gmail.com>
2023-10-19 09:42:35 +0500

    Till Testimonials Section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+    },
+});
